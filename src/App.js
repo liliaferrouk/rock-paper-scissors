2bc41024a82f447a466f6764279ac4e51629c70e
liliaferrouk <ferrouklilia@gmail.com>
2024-06-01 19:27:30 +0200

    the end <3

--- src/App.js
+++ src/App.js
@@ -44,6 +44,15 @@ function App() {
     setEndOfGame(false)
   }
 
+  useEffect(() => {
+    if (navigator.userAgent.match(/samsung/i)) {
+      alert("Your browser (Samsung Internet) may not show this website" +
+            " correctly. Please consider using a standards-compliant" +
+            " browser instead. \n\n" +
+            "We recommend Firefox, Microsoft Edge, or Google Chrome.");
+    }
+  }, []);
+
   return (
     <div className="App">
           <Header score={score}/>
